500f79ece828f5e2a473b304a3b3964379c862d8
pedro.jardim <pedro.d.jardim@hotmail.com>
2022-04-23 19:27:17 -0300

    feature: ddl de criacao da tabela livro

---
+++ src/backend/migrations/create_livros_v1.sql
@@ -0,0 +1,12 @@
+CREATE TABLE livro (
+    id INT PRIMARY KEY,
+    titulo VARCHAR(255),
+    autor VARCHAR(255),
+    ano_publicacao VARCHAR(255),
+    sinopse VARCHAR(255),
+    edicao VARCHAR(255),
+    editora VARCHAR(255),
+    isbn INT,
+    img_link VARCHAR(255)
+);
+
